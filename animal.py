cd51de1b14044566d53afec9e1d955262fc0204e
aizaz <aizaz>
2022-10-05 00:06:01 +0500

    add dog functionality

--- animal.py
+++ animal.py
@@ -1,1 +1,14 @@
-print ("This is animal python")
+import sys
+def default():
+    print('hello')
+
+def dog():
+      print('Woof!')
+
+def main():
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
+if __name__ == '__main__':
+    main()
